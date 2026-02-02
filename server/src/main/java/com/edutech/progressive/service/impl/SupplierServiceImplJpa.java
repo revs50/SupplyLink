81473f1b5c0b5503f5ca2c64cdd3d0928157db40
revs50 <136578432+revs50@users.noreply.github.com>
2026-02-02 21:07:52 +0530

    Update SupplierServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/SupplierServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/SupplierServiceImplJpa.java
@@ -1,5 +1,45 @@
 package com.edutech.progressive.service.impl;
 
-public class SupplierServiceImplJpa  {
+import com.edutech.progressive.entity.Supplier;
+import com.edutech.progressive.service.SupplierService;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupplierServiceImplJpa implements SupplierService {
+
+    @Override
+    public List<Supplier> getAllSuppliers() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public int addSupplier(Supplier supplier) {
+        return -1;
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliersSortedByName() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public void updateSupplier(Supplier supplier) {
+        // placeholder
+    }
+
+    @Override
+    public void deleteSupplier(int supplierId) {
+        // placeholder
+    }
+
+    @Override
+    public Supplier getSupplierById(int supplierId) {
+        return null;
+    }
+
+    @Override
+    public void emptyArrayList() {
+        // not applicable here
+    }
 }
