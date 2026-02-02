2c84192d118915eb53c7121169057e3404ad131a
revs50 <136578432+revs50@users.noreply.github.com>
2026-02-02 21:09:30 +0530

    Update WarehouseServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/WarehouseServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/WarehouseServiceImplJpa.java
@@ -1,5 +1,49 @@
 package com.edutech.progressive.service.impl;
 
-public class WarehouseServiceImplJpa  {
+import com.edutech.progressive.entity.Warehouse;
+import com.edutech.progressive.service.WarehouseService;
 
+import java.util.List;
+
+public class WarehouseServiceImplJpa implements WarehouseService {
+
+    @Override
+    public List<Warehouse> getAllWarehouses() {
+        return List.of();
+    }
+
+    @Override
+    public int addWarehouse(Warehouse warehouse) {
+        return -1;
+    }
+
+    @Override
+    public List<Warehouse> getWarehousesSortedByCapacity() {
+        return List.of();
+    }
+
+    @Override
+    public void updateWarehouse(Warehouse warehouse) {
+        // placeholder
+    }
+
+    @Override
+    public void deleteWarehouse(int warehouseId) {
+        // placeholder
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId) {
+        return null;
+    }
+
+    @Override
+    public List<Warehouse> getWarehouseBySupplier(int supplierId) {
+        return null;
+    }
+
+    @Override
+    public void emptyArrayList() {
+        // not applicable for JPA
+    }
 }
