b0125d6947aacf5616db713e0b2ed0a23bee1bb3
revs50 <136578432+revs50@users.noreply.github.com>
2026-02-05 02:12:11 +0530

    Update ProductServiceImplJpa.java

--- server/src/main/java/com/edutech/progressive/service/impl/ProductServiceImplJpa.java
+++ server/src/main/java/com/edutech/progressive/service/impl/ProductServiceImplJpa.java
@@ -1,16 +1,10 @@
 package com.edutech.progressive.service.impl;
 
 import com.edutech.progressive.entity.Product;
-import com.edutech.progressive.entity.Warehouse;
-import com.edutech.progressive.exception.InsufficientCapacityException;
 import com.edutech.progressive.repository.ProductRepository;
-import com.edutech.progressive.repository.ShipmentRepository;
-import com.edutech.progressive.repository.WarehouseRepository;
 import com.edutech.progressive.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -18,52 +12,38 @@ public class ProductServiceImplJpa implements ProductService {
 
     private final ProductRepository productRepository;
 
-    @Autowired
-    WarehouseRepository warehouseRepository;
-
-    @Autowired
-    ShipmentRepository shipmentRepository;
-
-    @Autowired
     public ProductServiceImplJpa(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     @Override
-    public List<Product> getAllProducts() throws SQLException {
+    public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
     @Override
-    public Product getProductById(int productId) throws SQLException {
-        return productRepository.findByProductId(productId);
+    public Product getProductById(int productId) {
+        return productRepository.findById(productId).orElse(null);
     }
 
     @Override
-    public int addProduct(Product product) throws InsufficientCapacityException {
-        Warehouse warehouse = warehouseRepository.findByWarehouseId(product.getWarehouse().getWarehouseId());
-        int productCount = productRepository.countByWarehouse_WarehouseId(warehouse.getWarehouseId());
-        if (warehouse.getCapacity() == productCount) {
-            throw new InsufficientCapacityException(
-                    "Warehouse with ID " + warehouse.getWarehouseId() + " has reached its maximum capacity of " + warehouse.getCapacity() + " products."
-            );
-        }
-        return productRepository.save(product).getProductId();
+    public int addProduct(Product product) {
+        Product saved = productRepository.save(product);
+        return saved.getProductId();
     }
 
     @Override
-    public void updateProduct(Product product) throws SQLException {
-        productRepository.save(product).getProductId();
+    public void updateProduct(Product product) {
+        productRepository.save(product);
     }
 
     @Override
-    public void deleteProduct(int productId) throws SQLException {
-        shipmentRepository.deleteByProductId(productId);
+    public void deleteProduct(int productId) {
         productRepository.deleteById(productId);
     }
 
     @Override
-    public List<Product> getAllProductByWarehouse(int warehouseId) throws SQLException {
-        return productRepository.findAllByWarehouse_WarehouseId(warehouseId);
+    public List<Product> getAllProductByWarehouse(int warehouseId) {
+        return List.of(); // placeholder as instructed
     }
 }
